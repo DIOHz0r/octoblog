f6f2fa4f10197d3908fe43bd30332e6911e408c5
Domingo Oropeza <dioh_@hotmail.com>
2020-01-31 13:58:58 -0300

    feature: autologin after registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,18 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Usuario;
 use App\Form\RegistrationFormType;
+use App\Security\LoginFormAutenticadorAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/registro", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        GuardAuthenticatorHandler $guardAuthenticatorHandler,
+        LoginFormAutenticadorAuthenticator $loginFormAutenticadorAuthenticator
+    ): Response
     {
         $user = new Usuario();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -23,6 +30,7 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
+            $user->setRoles(['ROLE_USER']);
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
@@ -35,8 +43,17 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
+            $this->addFlash(
+                'success',
+                'Usuario registrado con exito, bienvenido(a)!'
+            );
 
-            return $this->redirectToRoute('usuario_show', ['id' => $user->getId()]);
+            return $guardAuthenticatorHandler->authenticateUserAndHandleSuccess(
+                    $user,
+                    $request,
+                    $loginFormAutenticadorAuthenticator,
+                    'main'
+                );
         }
 
         return $this->render('registration/register.html.twig', [
